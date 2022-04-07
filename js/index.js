ca2b9d8a73ca110e8cc8253863a7addc3914a34c
rbidon <roussebidon@gmail.com>
2022-04-07 18:19:44 -0400

    Comments+ Linked the index.html to navbar

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
 console.log("Connected!");
+// testing code: new instance of taskManager
+// use addtask method to add a new task to the manager
+// must return an empty arry in the brower
+// let newTask = new taskManager();
+// newTask.addtask();
+// console.log(newTask.addtask);
 
+
+// //  declare taskManager to a variable
+// // const taskCollection = new taskManager();
+// // Task 4:Step 4 - Adding Tasks with the form
+const addTaskForm = document.querySelector('#addTaskForm');
+// - Display the form html
+console.log(addTaskForm); 
+const addTaskbutton = document.getElementById('#addTask-button');
+// // add an onsubmit event listener for form
+addTaskForm.addEventListener('submit',  (event) => {
+    // prevent default method
+    event.preventDefault();
+    
+    // Add the task to the task manager
+    taskManager.addtask(name,description,assignedTo,dueDate,status);
+});
+// // Task 5: Tested Code - Step 1  Worked well😁
+// let taskHtml = createTaskHTML();
+// console.log(taskHtml);
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,26 +1,75 @@
 console.log("Connected");
-class  TaskManager {
-    constructor(currentId) { 
-        this.task = [];
-        this.currentId = 0; 
+// create task HTML function
+// let createTaskHTML = (name,description,assignedTo,dueDate,status) => {
+//     // string using template literals
+//     const html = ` <div class="card col-sm border border-dark" style="width: 18rem; background-color:#f5ececab;">
+//     <div class="card-body">
+//       <h3 class="card-title">${name}</h3>
+//       <p class="card-text">${description}</p>
+//       <a href="#" class="btn btn-primary">Due Date: ${dueDate}</a>
+//   </div> `;
+//      console.log(html);
+//      return html;
+// };
+
+// 
+class  taskManager {
+    constructor(currentId = 0) { 
+        this.tasks = [];
+        this.currentId = currentId; 
     }
-    
+    // create the addTask method
     addtask(name,description,assignedTo,dueDate,status){
-   const task = {
-       id: this.currentId++,
-    //    name : ,
-    //    description: ,
-    //    assignedTo: ,
-    //    dueDate: ,
-    //    status: 
-   }
 
-   this.task.push(task);
-   console.log(this.task);
-}; 
+        const task = {
+             id: this.currentId++,
+             name: name,
+             description: description,
+             assignedTo: assignedTo,
+             dueDate: dueDate,
+             status: status
+        };
 
 
+        this.tasks.push(task);
+        console.log(task);
+   }  
+    
+//    Step 2: render method() used to display task on the page
+//    render()  {
+//        const taskHtmlList = [];
+//     for(let i = 0 ; i <this.tasks.length; i++){ 
+//        const currentTask = this.tasks[i];
+//         const newDate = new Date(currentTask.dueDate);
+//         // styling how the date will appear 
+//         const formattedDate = (newDate.getMonth() + 1) + "/" + newDate.getDate() + "/" + newDate.getFullYear();
+//         const taskHtml = createTaskHTML(currentTask.name, currentTask.description,currentTask.assignedTo, formattedDate, currentTask.status);
+        
+//         taskHtmlList.push(taskHtml);
+//         console.log(taskHtmlList);
+//     }
+    // seperate w. newline 
+    // const tasksHtml = taskHtmlList.join('\n');
+    // const tasksList = document.querySelector('#inprogressTask');
+    // tasksList.innerHTML = tasksHtml;
+       //  display taskHTMLlist in different caterogy 
+    // create if statement 
+    // if( document.getElementById("inprogress").selected = "true"){ 
+    //     const inprogressTask =document.getElementById('#inprogressTask');
+    //     inprogress.innerHTML = tasksHtml;
+    // } else if ( document.getElementById("#upcoming").selected = "true") {
+    //     const upcoming = document.querySelector('#upcomingTask');
+    //     upcoming.innerHTML = tasksHtml;
+    
+    // } else if ( document.getElementById) { 
+    //     const completedTask = document.querySelector('#completedTask');
+    //      completedTask.innerHTML = tasksHtml;
 
+    // }
+    
+   
+    // }
+}
 
 
 //     this.name =name;
@@ -30,7 +79,7 @@ class  TaskManager {
 //     this.status = status;
 //    ) { 
 
-}
+
     // // getters()
     //  get id () { return this.id }
     // get name () { return this.name }
